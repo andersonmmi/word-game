b6430e120262b4e5d4d7da799e45c5452ed2eb53
Aaron Anderson <aaronlynanderson@gmail.com>
2017-08-23 13:45:08 -0400

    App serves index.mustache

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ app.set('views','./views');
 app.use(parser.json());
 app.use(parser.urlencoded({extended : false}));
 
+// TODO: session does not create an object yet
+app.use(session({
+  secret : 'admin',
+  resave : false,
+  saveUnitialized : true
+}));
+
+app.get('/',function(req, res){
+  res.render('index');
+});
+
 app.listen(port, function(){
   console.log('listening on ' + port);
+  console.log(session.object);
 });
