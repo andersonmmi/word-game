8e2a8e5cf7d5ab73537bd35adc36dcfa1e6a940b
Aaron Anderson <aaronlynanderson@gmail.com>
2017-08-23 13:34:22 -0400

    Establish initial dependencies

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 const express = require('express');
+const app = express();
+const express = require('mustache-express');
+const parser = require('body-parser');
+const port = 3000;
+const session = require('express-session');
+
+app.engine('mustache',mustache());
+app.set('view engine', 'mustache');
+app.set('views','./views');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.listen(port, function(){
+  console.log('listening on ' + port);
+});
